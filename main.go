4190fc5e28a717625a8762de23e9ca15cec14f06
Dunitt Monagas <dunitt@example.com>
2022-06-21 02:48:58 -0400

    Go Tutorial Series EP.2: Persisting API Data with Gorm

--- main.go
+++ main.go
@@ -1,59 +1,72 @@
-
 package main
 
 import (
 	"net/http"
-	"github.com/gin-gonic/gin"
-)
 
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
 
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect database.")
+	}
+
+	db.AutoMigrate(&Book{})
+
+	handler := newHandler(db)
+
 	r := gin.New()
 
-	r.GET("/", helloHandler)
+	r.GET("/books", handler.listBooksHandler)
 
-	r.GET("/books", listBooksHandler)
-	
-	r.POST("/books", createBookHandler)
+	r.POST("/books", handler.createBookHandler)
 
-	r.DELETE("/books/:id", deleteBookHandler)
+	r.DELETE("/books/:id", handler.deleteBookHandler)
 
 	r.Run()
 
 }
 
+type Handler struct {
+	db *gorm.DB
+}
+
+func newHandler(db *gorm.DB) *Handler {
+	return &Handler{db}
+}
+
 type Book struct {
-	ID string `json:"id"`
-	Title string `json:"title"`
+	ID     string `json:"id"`
+	Title  string `json:"title"`
 	Author string `json:"author"`
 }
 
-var books = []Book {
-	{ID: "1", Title: "Harry Potter", Author: "J. K. Rowling"},
-	{ID: "2", Title: "The Lord of the Rings", Author: "J. R. R. Tolkien"},
-	{ID: "3", Title: "The Wizard of Oz", Author: "L. Frank Baum"},
+func (h *Handler) listBooksHandler(c *gin.Context) {
+
+	var books []Book
+
+	if result := h.db.Find(&books); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": result.Error.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(http.StatusOK, &books)
+
 }
 
-
-
-func helloHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Hello Worl!",
-	})
-}
-
-
-func listBooksHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, books)
-}
-
-
-func createBookHandler(c *gin.Context) {
+func (h *Handler) createBookHandler(c *gin.Context) {
 
 	var book Book
 	if err := c.ShouldBindJSON(&book); err != nil {
-		
+
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
 		})
@@ -61,26 +74,32 @@ func createBookHandler(c *gin.Context) {
 		return
 	}
 
-	books = append(books, book)
+	if result := h.db.Create(&book); result.Error != nil {
 
-	c.JSON(http.StatusCreated, book)
+		c.JSON(http.StatusInsufficientStorage, gin.H{
+			"error": result.Error.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(http.StatusCreated, &book)
 
 }
 
-func deleteBookHandler(c *gin.Context) {
+func (h *Handler) deleteBookHandler(c *gin.Context) {
 
 	id := c.Param("id")
 
-	for i, a := range books {
-		if a.ID == id {
-			books = append(books[:i], books[i  + 1:]...)
-			break
-		}
+	if result := h.db.Delete(&Book{}, id); result.Error != nil {
+
+		c.JSON(http.StatusInsufficientStorage, gin.H{
+			"error": result.Error.Error(),
+		})
+
+		return
 	}
 
 	c.Status(http.StatusNoContent)
 
 }
-
-
-
